c694c281f7920a3afb7e4a0c1050d610af97e76e
sonika <sonikasg1402@gmail.com>
2024-09-23 00:25:35 +0530

    styling

--- lorem/src/components/Main.js
+++ lorem/src/components/Main.js
@@ -3,9 +3,10 @@ class Main extends React.Component{
     render(){
     return(
         <>
-        <div style={{height:"550px",
+        <div style={{height:"590px",
             width:"50%",
             display:"flex",
+            overflow: "hidden"
             }}>
                 
         <img src="/pexels-robin-stickel-70497.jpg"></img>
--- lorem/src/components/Navbar.js
+++ lorem/src/components/Navbar.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav style={{backgroundColor:"#C75B7A", display:"flex",justifyContent:"space-around" , padding:"10px"
+    <nav style={{height:"25px",backgroundColor:"#C75B7A", display:"flex",justifyContent:"space-around" , padding:"10px"
     }}>
-      <ul style={{display:"flex",listStyle:"none",gap:"20px"}}>
+      <ul style={{display:"flex",listStyle:"none",gap:"20px",
+         padding: 0, 
+         margin: 0, 
+         
+      }}>
         <li>Home</li>
         <li>Order</li>
         <li>Food</li>
         <li>Restaurant</li>
         <li>Testimonials</li>
         <li>Contact Us</li>
+        <img src='/menu-bar.png'></img>
       </ul>
     </nav>
   );
