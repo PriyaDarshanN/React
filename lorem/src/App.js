0c6c1f134809e397a66fd2d752cb881e5597d4d3
PriyaDarshan N <96416309+PriyaDarshanN@users.noreply.github.com>
2024-09-22 19:05:51 +0000

    lorem

--- lorem/src/App.js
+++ lorem/src/App.js
@@ -14,7 +14,6 @@ function App() {
       <Main>
       </Main>
       </div>
-      <Testimonials />
       <Footer />
     </div>
   );
--- lorem/src/components/HeroSection.js
+++ lorem/src/components/HeroSection.js
@@ -16,7 +16,12 @@ function HeroSection() {
 Quisque congue sem nec justo mollis, in tincidunt erat pretium.
 Sed pulvinar, massa ac porta viverra.
 </p>
-      <button>Click Me</button>
+      <button style={{padding: "10px 20px",
+    backgroundColor: "#C75B7A",
+    color: "white",
+    border: "none",
+    position:"centre",
+    cursor: "pointer"}}>Click Me</button>
     </section>
   );
 }
